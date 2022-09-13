044d981ab4d1b01f5f87c016c26005706a789edf
Pradeepkumar <pradeep@shoptrade.co>
2022-09-13 12:13:36 +0530

    Mega menu added for header

--- assets/custom.js
+++ assets/custom.js
@@ -48,6 +48,19 @@ function hideVideo(index, e) {
 
 
 $(document).ready(function () {
+  let items = document.querySelectorAll('.menu-mega-nav li');
+
+  items.forEach( item => item.addEventListener('mouseenter', function() {
+    handleHover(this, items)    
+  }))
+
+  function handleHover(el, objects) {
+    items.forEach(item => {
+      item.classList.remove('active')      
+    })
+    el.classList.add('active')
+  }
+ 
   theme.icons = {    
     chevronLeft: '<svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M11.913,23.827l-1.564-1.6,9.2-9.2H0V10.8H19.545l-9.2-9.2L11.913,0,23.827,11.913Z" transform="translate(23.827 23.827) rotate(180)"/></svg>',
     chevronRight: '<svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M19.913,31.827l-1.564-1.6,9.2-9.2H8V18.8H27.545l-9.2-9.2L19.913,8,31.827,19.913Z" transform="translate(-8 -8)"/></svg>',
