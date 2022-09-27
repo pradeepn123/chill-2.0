1c5d76fe5a7057b0d54062aaff0d32b9da332a8f
Pradeepkumar <pradeep@shoptrade.co>
2022-09-27 10:44:40 +0530

    product page development with subscription

--- assets/custom.js
+++ assets/custom.js
@@ -49,9 +49,8 @@ function hideVideo(index, e) {
 
 $(document).ready(function () {
   let items = document.querySelectorAll('.menu-mega-nav li');
-
   items.forEach( item => item.addEventListener('mouseenter', function() {
-    handleHover(this, items)    
+    handleHover(this, items)
   }))
 
   function handleHover(el, objects) {
@@ -60,6 +59,13 @@ $(document).ready(function () {
     })
     el.classList.add('active')
   }
+  $('.custom_sub_button').click(function(){
+    $(this).closest('.block-inner').find('.block-inner-card-info').addClass('add_info_sub');
+  });
+  $('.close_card_info').click(function(){
+    $(this).closest('.block-inner').find('.block-inner-card-info').removeClass('add_info_sub');
+  });
+  
   //sub collection image
   $('.subcollection_slider').slick({
     dots: false,
@@ -100,9 +106,9 @@ $(document).ready(function () {
             slidesToScroll: 1
         }
         }
-    ]
-});
-$(".subcollection_slider").show();
+      ]
+  });
+  $(".subcollection_slider").show();
 
   theme.icons = {    
     chevronLeft: '<svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M11.913,23.827l-1.564-1.6,9.2-9.2H0V10.8H19.545l-9.2-9.2L11.913,0,23.827,11.913Z" transform="translate(23.827 23.827) rotate(180)"/></svg>',
@@ -163,13 +169,6 @@ $(".subcollection_slider").show();
       });
   }
   scrollAbout();
-
-  $('.custom_sub_button').click(function(){
-    $(this).closest('.block-inner').find('.block-inner-card-info').addClass('add_info_sub');
-  });
-  $('.close_card_info').click(function(){
-    $(this).closest('.block-inner').find('.block-inner-card-info').removeClass('add_info_sub');
-  });
   
   $('.add_to_cart_btn').click(function(){    
     addItemToCart( $(this).attr("data-product-id") , 1)
@@ -195,14 +194,16 @@ $(".subcollection_slider").show();
     	bubbles: true 
  	 }));
   }
-
-  
 });
+
 (function(){
   var d = document,
       tabs = d.querySelector('.tabs'),
       tab = d.querySelectorAll('li'),
       contents = d.querySelectorAll('.content');
+      if (!tabs) {
+        return false;
+      }
       tabs.addEventListener('click', function(e) {
       if (e.target && e.target.nodeName === 'LI') {
         // change tabs
--- assets/theme.js
+++ assets/theme.js
@@ -3055,6 +3055,14 @@
     });
   };
 
+  theme.handleBuyNow = function(element) {
+    element.setAttribute("disabled", true)
+    element.innerHTML = "Loading"
+    setTimeout(function() {
+      window.location.href = `/cart/${element.dataset.variantId}:${element.dataset.quantity}?traffic_source=buy_now`
+    })
+  }
+
   theme.icons = {
     left: '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><title>' + theme.strings.icon_labels_left + '</title><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>',
     right: '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><title>' + theme.strings.icon_labels_right + '</title><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg>',
