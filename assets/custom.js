8dce6f7a0becb95201d158fcf08bfc2b2ee04540
Keerthi-Sindhura <keerthi@shoptrade.co>
2023-05-03 15:33:31 +0530

    Optimised speed of marquee on scroll

--- assets/custom.js
+++ assets/custom.js
@@ -970,21 +970,7 @@ class AutoSlider {
 //     .off("click", theme.closeDrawerCart.bind(theme))
 // };
 // document.querySelector('.utility-bar .link-dropdown__button').removeAttribute('disabled');
-var marqueeAnimations = document.querySelectorAll('.marquee_container.animate-with-scroll');
-window.addEventListener('wheel', evt => {
-    evt.preventDefault();
-    marqueeAnimations.forEach(marqueeAnimation => {
-        evt.preventDefault();
-        if(evt.deltaY > 0){
-            marqueeAnimation.classList.remove('scroll_up');
-            marqueeAnimation.classList.add('scroll_down');
-        }
-        else if(evt.deltaY <= 0){
-            marqueeAnimation.classList.remove('scroll_down');
-            marqueeAnimation.classList.add('scroll_up');
-        }
-    })
-})
+
 
 
 function openWaitlistDrawer(){
@@ -1053,3 +1039,28 @@ function validateForm(e){
 //         // document.querySelector('.flavour_image.secondary').classList.remove('secondary');
 //     })
 // })
+
+	document.addEventListener("scroll", function() {
+            getProgress();
+	});
+
+	function getProgress() {					
+		var topPos = document.documentElement.scrollTop;
+		// Alternatively, you can use document.body.scrollTop || document.documentElement.scrollTop;
+		
+		// Remaining left to scroll
+		var remaining = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+		// scrollHeight is the measurement of the element's entire content, whether all the content is visible or not
+		// clientHeight is the inner height of the element, including padding
+		
+		var percentage = (topPos / remaining) * 100 * 2;
+            
+        document.querySelectorAll('.marquee_container .marquee_content_wrapper').forEach((el, index) => {
+            if (index >= 10 ) {
+                return el.style.transform = 'translateX(-' + percentage + '%)';
+            }
+            else{
+                return el.style.transform = 'translateX(-' + percentage/2 + '%)';
+            }
+        })
+    }
