19a0a1cff39226f6af35ade2efbd4ad4d0da682f
Keerthi-Sindhura <keerthi@shoptrade.co>
2023-05-04 17:24:45 +0530

    Removed sticky add-to-cart when 'you may also like' section appears

--- assets/custom.js
+++ assets/custom.js
@@ -1012,6 +1012,16 @@ document.querySelectorAll('.mobile_popup_buy_button').forEach(popupButton => {
         buyButtonContainer.classList.add('buy_buttons_show');
     })
 })
+window.addEventListener('scroll', () => {
+    document.querySelectorAll('.product-recommendations').forEach(product_recommendation => {
+        if(window.scrollY + window.innerHeight - 100 > product_recommendation.offsetTop){
+            buyButtonContainer.parentElement.style.display = 'none';
+        }
+        else{
+            buyButtonContainer.parentElement.style.display = 'block';
+        }
+    })
+})
 
 function openWaitlistDrawer(){
     document.getElementById('waitlistDrawerContainer').style.display='block'; 
